0278429629ec8d093e344e08d400ea0b7e4241ea
jamesbandenburg <77491156+jamesbandenburg@users.noreply.github.com>
2021-06-07 17:31:17 +0100

    Fixing login issue with deployed app

--- client/src/layout/Login.js
+++ client/src/layout/Login.js
@@ -24,8 +24,8 @@ const Login = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log(users)
-        const userToLogin = users.map((ele,index) =>  { if (ele.length > 0) { return ele[index].filter((elem) => elem.email === loginEmail)}  })
+
+        const userToLogin = users[0].filter(user => user.email === loginEmail)
        
         if (userToLogin.length === 0) {
             alert("User not found. Please register if you don't have an account.")
